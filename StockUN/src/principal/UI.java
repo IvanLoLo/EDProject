1f7bc0404161eb76fafbeecf54aa01d9e0a342cf
IvanLoLo <ilombana@unal.edu.co>
2020-10-31 16:56:42 -0500

    Prueba branch

---
+++ StockUN/src/principal/UI.java
@@ -0,0 +1,14 @@
+
+package principal;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class UI {
+    
+    public static void main(String[] args) {
+        System.out.println("Probando");
+    }
+
+}
